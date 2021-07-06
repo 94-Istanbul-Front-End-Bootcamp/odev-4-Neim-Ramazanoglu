cbedce5d268c36018890f55c8a8c7a7400489d16
Neim-Ramazanoglu <mr.ramazanoglu@gmail.com>
2021-07-07 00:56:08 +0300

    stars changed

--- rating-app/src/App.js
+++ rating-app/src/App.js
@@ -7,52 +7,14 @@ import  { Component } from 'react';
 class App extends Component{
   constructor(props) {
     super(props);
-    this.state = {
-      userInput: "",
-      text: [],
-      disabled: true,
-    };
     
   }
-  onInputChange = (e) => {
-    debugger;
-    
-    const newVal = e.target.value;
-    this.setState({
-      userInput: newVal,
-    });
-    if (newVal.length >= 3 ) this.state.disabled=false;
-    else this.state.disabled=true;
-  };
-   
-  btnYorum =(e) => {
-    
-    this.setState(
-      {
-      text: [...this.state.text, this.state.userInput],
-      
-    },
-    () => {
-      this.setState({
-        userInput: "",
-      });
-    })
-  };
-
-
   
   render() {
   return (
     <div className="App">
       <Yorum />
-      <List
-      userInput={this.state.userInput}
-      onInputChange={this.onInputChange}
-      btnYorum={this.btnYorum}
-      text={this.state.text}
-      disabled = {this.state.disabled}
-      />
-      
+      <List />
     </div>
   );
 }
--- rating-app/src/components/ListComp/List.js
+++ rating-app/src/components/ListComp/List.js
@@ -2,13 +2,31 @@ import ReactStars from "react-rating-stars-component";
 import React, { useState } from 'react';
 
 import "./List.css";
-const List = ({ userInput, onInputChange, btnYorum, text,disabled, stars }) => {
+const List = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
   };
   const [editing, setEditing] = useState(0);
   const ratingChanged = (newRating) => {
         setEditing(newRating);
+        if (userInput.length >= 3 && newRating != 0 ) setDisabled(false);
+        else setDisabled(true);
+  };
+
+  const [userInput, setUserInput] = useState("");
+  const [disabled, setDisabled] = useState(true);
+  const onInputChange = (e) => {
+    const newVal = e.target.value;
+    setUserInput(newVal);
+    if (newVal.length >= 3 && editing != 0 ) setDisabled(false);
+    else setDisabled(true);
+  };
+
+  const [text, setText] = useState([]);
+ const btnYorum =(e) => {
+    setText([...text, userInput])
+    setUserInput("");
+    //
   };
   return (
     <div>
