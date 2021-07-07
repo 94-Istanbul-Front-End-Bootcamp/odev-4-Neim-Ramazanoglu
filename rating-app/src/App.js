f600b7ec7b164a59262850940e907804bdbee54b
Neim-Ramazanoglu <mr.ramazanoglu@gmail.com>
2021-07-07 17:41:49 +0300

    last commit

--- rating-app/src/App.js
+++ rating-app/src/App.js
@@ -3,18 +3,60 @@ import Yorum from './components/Yorum/Yorum'
 import List from './components/ListComp/List';
 import  { Component } from 'react';
 
-   
+
+
 class App extends Component{
-  constructor(props) {
-    super(props);
-    
+  state = {
+    comments: [],
+    commentValue: "",
+    vote: 0,
+    id: 0
   }
+  handleComment = () => {
+    
+    const newState = this.state;
+  
+    newState.comments.push({ 
+        id: Math.random(),
+        text: this.state.commentValue, 
+        vote: this.state.vote,
+         
+    });
+    newState.commentValue = "";
+    newState.vote = 0;
+    newState.id = 0;
+  
+    this.setState({
+      ...newState
+    });
+  }
+  
+
+   ratingChanged = (newRating) => {
+    if (newRating > 0) {
+      this.setState({vote: newRating})
+    }
+
+  };
+  
+   yorum = () => {
+    let x = document.getElementById("yorum").value;
+  
+    if (x.length > 3) {
+      this.setState({commentValue: x})
+
+    }
+  };
   
   render() {
   return (
     <div className="App">
       <Yorum />
-      <List />
+      <List 
+      handleComment={this.handleComment}
+      comments={this.state.comments}
+      ratingChanged={this.ratingChanged}
+      yorum = {this.yorum}/>
     </div>
   );
 }
--- rating-app/src/components/ListComp/List.js
+++ rating-app/src/components/ListComp/List.js
@@ -1,69 +1,65 @@
 import ReactStars from "react-rating-stars-component";
-import React, { useState } from 'react';
 
 import "./List.css";
-const List = () => {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-  };
-  const [editing, setEditing] = useState(0);
-  const ratingChanged = (newRating) => {
-        setEditing(newRating);
-        if (userInput.length >= 3 && newRating != 0 ) setDisabled(false);
-        else setDisabled(true);
-  };
 
-  const [userInput, setUserInput] = useState("");
-  const [disabled, setDisabled] = useState(true);
-  const onInputChange = (e) => {
-    const newVal = e.target.value;
-    setUserInput(newVal);
-    if (newVal.length >= 3 && editing != 0 ) setDisabled(false);
-    else setDisabled(true);
-  };
+function List(props) {
+  //   const handleSubmit = (event) => {
+  //     event.preventDefault();
+  //   };
+  // const [editing, setEditing] = useState(0);
+  const { handleComment, ratingChanged, yorum, comments } = props;
 
-  const [text, setText] = useState([]);
- const btnYorum =(e) => {
-    setText([...text, userInput])
-    setUserInput("");
-    //
-  };
+  //   const [userInput, setUserInput] = useState("");
+  //   const onInputChange = (e) => {
+  //     const newVal = e.target.value;
+  //     setUserInput(newVal);
+
+  //   };
+
+  //  const [text, setText] = useState([]);
+  //  const btnYorum =(e) => {
+  //     setText([...text, userInput])
+  //     setUserInput("");
+  //     //
+  //   };
 
   return (
     <div>
-      <form className="input" onSubmit={handleSubmit}>
-      
-        <h3>{text.length === 0 ? "Henüz hiç yorum yok." : ""}</h3>
-        {text.map((a, i) => {
+      <ul>
+        <h3>{comments.length === 0 ? "Henüz hiç yorum yok." : ""}</h3>
+        {comments.map((comment) => {
           return (
-            <div >
-              <p>{a}</p>
-              <p className="List"><ReactStars
-            count={5}
-            onChange={(ratingChanged)}
-            size={30}
-            activeColor="#ffd700"
-            value={editing}
-          /></p>
+            <div>
+              <li key={comment.id}>{comment.text}</li>
+              <li className="List">
+                <ReactStars
+                  count={5}
+                  edit={false}
+                  size={30}
+                  activeColor="#ffd700"
+                  value={comment.vote}
+                />
+              </li>
               <br />
             </div>
           );
         })}
-       
-        <input type="text" value={userInput} onChange={onInputChange} /><br></br>
-        
-        <div className="stars">
-      <ReactStars
-            count={5}
-            onChange={(ratingChanged)}
-            size={40}
-            activeColor="#ffd700"
-          />
-            </div><br></br>
-        <button onClick={btnYorum} disabled={disabled}>Gönder</button>
-      </form>
+      </ul>
+      <input type="text" onChange={yorum} id="yorum" />
+      <br></br>
+
+      <div className="stars">
+        <ReactStars
+          count={5}
+          onChange={ratingChanged}
+          size={40}
+          activeColor="#ffd700"
+        />
+      </div>
+      <br></br>
+      <button onClick={handleComment}>Gönder</button>
     </div>
   );
-};
+}
 
 export default List;
